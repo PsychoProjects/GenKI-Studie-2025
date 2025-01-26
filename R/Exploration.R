af93b79d9ab207cdf978b63eba63f660a0cc7710
Achim <achim@farbstuecke.de>
2025-01-26 16:14:13 +0100

    Akzeptanzanalysen

--- R/Exploration.R
+++ R/Exploration.R
@@ -3,13 +3,19 @@ source("Read_Data.R")
 
 ### Analysen zur Verteilung bezüglich der Geschlechter
 # Statistiken nach Geschlecht
-daten %>% group_by(Geschlecht) %>% count()
+daten %>% group_by(Geschlecht) %>% skim(Akzeptanz)
 daten %>% group_by(Geschlecht, Gruppe) %>% skim(Akzeptanz)
 
 ### Analysen zur Verteilung bezüglich der Bildungsabschlüsse
 # Statistiken nach Bildungsabschluss
-daten %>% group_by(Bildungsabschluss) %>% count()
+daten %>% group_by(Bildungsabschluss) %>% skim(Akzeptanz)
 daten %>% group_by(Bildungsabschluss, Gruppe) %>% skim(Akzeptanz)
+
+### Analysen zur Verteilung bezüglich der Berufserfahrung
+# Statistiken nach Berufserfahrung
+daten %>% group_by(Berufserfahrung) %>% skim(Akzeptanz)
+daten %>% group_by(Berufserfahrung, Gruppe) %>% skim(Akzeptanz)
+
 
 ### Analysen zur Einschätzung der Objektivität bzw. Subjektivität der Anwendungsfelder
 # Korrelationsanalyse zwischen Anwendungsfeld und objektiv-subjektiv-Einschätzung
