382eeb271c05fcaa1433bff1bd82776b972d1080
Achim <achim@farbstuecke.de>
2025-02-10 20:11:20 +0100

    Analysen Strukturiert

--- R/Analyze.R
+++ R/Analyze.R
@@ -1,11 +1,19 @@
 source("RunScript.R")
 
+# Methoden-Teil
 run_script("Describe_Participants")
 run_script("Describe_Data")
-run_script("Acceptance")
+
+# Ergebnis-Teil
+## Hypothesen
 run_script("ANCOVA")
+run_script("H4-Analysis")
+
+## Confirmatorische Analyse
 run_script("Confirmation_analysis")
-run_script("PowerAnalysis")
-run_script("Exploration")
-run_script("Validity_Checks")
 run_script("Attari-12")
+
+## Explorative Analyse
+run_script("Anwendungsfeld_Gruppenvergleich")
+run_script("Exploration")
+
